39845f7144add09abb1c9e2d2684d6048415f823
Omprakash Patley <omprakashpatley72@gmail.com>
2021-10-23 21:52:56 +0530

    task manager app built

---
+++ task.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded',function(){
+function t_manager(){
+    document.querySelector('#submit').disabled = true;
+    document.querySelector('#add').onkeyup = () => {
+        if (document.querySelector('#add').value.length > 0) {
+        document.querySelector('#submit').disabled = false;
+        } else {
+        document.querySelector('#submit').disabled = true;
+    }
+}
+    const task = document.querySelector('#add').value;
+    const li = document.createElement('li');
+    li.innerHTML = task;
+    document.querySelector('#tasks').append(li);
+    document.querySelector('#add').value = '';
+    document.querySelector('#submit').disabled = true;
+    return false;
+}
+document.querySelector('form').onsubmit = t_manager;
+});
