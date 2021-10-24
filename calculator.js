5aa2e5a4e64a2aea2c8714475f1c441f093aa486
Omprakash Patley <omprakashpatley72@gmail.com>
2021-10-24 09:07:16 +0530

    updated methods

--- calculator.js
+++ calculator.js
@@ -2,11 +2,9 @@ let counter = 0;
         function count() {
             counter++;
             document.querySelector('h1').innerHTML =counter;
-            if (counter % 10=== 0) {                 // template literal
-            alert(`count is now ${counter}`)
-            } 
         }
         document.querySelector('button').onclick = count;
-        document.querySelector('button').addEventListener('mouseover',count);
+        setInterval(count,1000);
+        // document.querySelector('button').addEventListener('mouseover',count);
 
         
--- task.js
+++ task.js
@@ -1,20 +1,20 @@
-document.addEventListener('DOMContentLoaded',function(){
 function t_manager(){
     document.querySelector('#submit').disabled = true;
     document.querySelector('#add').onkeyup = () => {
         if (document.querySelector('#add').value.length > 0) {
-        document.querySelector('#submit').disabled = false;
+            document.querySelector('#submit').disabled = false;
         } else {
         document.querySelector('#submit').disabled = true;
     }
 }
-    const task = document.querySelector('#add').value;
-    const li = document.createElement('li');
-    li.innerHTML = task;
-    document.querySelector('#tasks').append(li);
-    document.querySelector('#add').value = '';
-    document.querySelector('#submit').disabled = true;
-    return false;
+const task = document.querySelector('#add').value;
+const li = document.createElement('li');
+li.innerHTML = task;
+document.querySelector('#tasks').append(li);
+document.querySelector('#add').value = '';
+document.querySelector('#submit').disabled = true;
+return false;
 }
+document.addEventListener('DOMContentLoaded',function(){
 document.querySelector('form').onsubmit = t_manager;
 });
